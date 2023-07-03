ac8f3b324a93adcbee221936e0d9a85bbb4e4af8
Andrei Bernatski <iandy1731@gmail.com>
2023-07-03 14:02:40 +0300

    feat: add solutions for functions tasks

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -8,7 +8,6 @@
  *                                                                                             *
  ********************************************************************************************* */
 
-
 /**
  * Returns the functions composition of two specified functions f(x) and g(x).
  * The result of compose is to be a function of one argument, (lets call the argument x),
@@ -23,10 +22,9 @@
  *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
  *
  */
-function getComposition(/* f, g */) {
-  throw new Error('Not implemented');
+function getComposition(f, g) {
+  return (x) => f(g(x));
 }
-
 
 /**
  * Returns the math power function with the specified exponent
@@ -44,10 +42,9 @@ function getComposition(/* f, g */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return (num) => num ** exponent;
 }
-
 
 /**
  * Returns the polynom function of one argument based on specified coefficients.
@@ -62,10 +59,18 @@ function getPowerFunction(/* exponent */) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
-}
+function getPolynom(...params) {
+  const arr = params.reverse();
+  return (x) => {
+    let result = 0;
 
+    for (let i = arr.length - 1; i >= 0; i -= 1) {
+      result += arr[i] * x ** i;
+    }
+
+    return result;
+  };
+}
 
 /**
  * Memoizes passed function and returns function
@@ -81,10 +86,11 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const cash = new Map();
+  cash.set(func, func());
+  return () => cash.get(func);
 }
-
 
 /**
  * Returns the function trying to call the passed function and if it throws,
@@ -101,10 +107,15 @@ function memoize(/* func */) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return () => {
+    try {
+      return func();
+    } catch (e) {
+      return retry(func, attempts - 1)();
+    }
+  };
 }
-
 
 /**
  * Returns the logging wrapper for the specified method,
@@ -129,10 +140,19 @@ function retry(/* func, attempts */) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
-}
+function logger(func, logFunc) {
+  return (...params) => {
+    const str = JSON.stringify(params).slice(1, -1);
 
+    logFunc(`${func.name}(${str}) starts`);
+
+    const result = func(...params);
+
+    logFunc(`${func.name}(${str}) ends`);
+
+    return result;
+  };
+}
 
 /**
  * Return the function with partial applied arguments
@@ -147,10 +167,9 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...params) {
+  return (...args) => fn(...params, ...args);
 }
-
 
 /**
  * Returns the id generator function that returns next integer starting
@@ -169,10 +188,21 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
-}
+function getIdGeneratorFunction(startFrom) {
+  const result = {
+    value: startFrom,
+    done: false,
+  };
 
+  return () => {
+    if (result.done) {
+      result.value += 1;
+      return result.value;
+    }
+    result.done = true;
+    return result.value;
+  };
+}
 
 module.exports = {
   getComposition,
